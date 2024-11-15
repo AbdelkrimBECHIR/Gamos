4a7948634281d047176fc9185f10c3c71c2c4653
Harold <harolddev75@gmail.com>
2024-11-15 16:18:29 +0100

    problem with flexbox arf

--- cars.php
+++ cars.php
@@ -4,8 +4,67 @@ $title = "cars";
 include("header.php");
 
 ?>
+<style>
+    .centertop {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between ;
+        height: 200px;
+        width: 1200vh;
+        align-items: center;
+        background-color: red;
+    }
+    </style>
+<!-- SELECT Bootstrap -->
+<div class="centertop">
+    <select class="form-select form-select-sm" aria-label="Small select example">
+        <option selected>Prix</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+    </select>
 
-
+    <select class="form-select form-select-sm" aria-label="Small select example">
+        <option selected>Marque</option>
+        <option value="1">Merco</option>
+        <option value="2">Féfé</option>
+        <option value="3">Lambo</option>
+    </select>
+    <select class="form-select form-select-sm" aria-label="Small select example">
+        <option selected>Carburant</option>
+        <option value="1">Essence SP 97</option>
+        <option value="2">Diesel</option>
+        <option value="3">Hydrogène</option>
+        <option value="4">Electrique</option>
+    </select>
+    <select class="form-select form-select-sm" aria-label="Small select example">
+        <option selected>Transmission</option>
+        <option value="1">Manuelle</option>
+        <option value="2">Automatique</option>
+    </select>
+    <select class="form-select form-select-sm" aria-label="Small select example">
+        <option selected>Siège</option>
+        <option value="1">2</option>
+        <option value="2">3</option>
+        <option value="3">4</option>
+        <option value="4">5</option>
+        <option value="5">6</option>
+        <option value="6">7</option>
+    </select> 
+</div>
+<!-- GRID bootstrap-->
+<div class="container center d-flex align-items-center justify-content-center">
+    <div class="container text-center">
+    <div class="row row-cols-3">
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+    </div>
+</div>
+</div>
 
 
 
